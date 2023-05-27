5e1035b56a75016b49e3c4a43f0851ef5aa37b4b
Nahiara Ricardini <134017331+NahiaraRicardini@users.noreply.github.com>
2023-05-27 11:00:26 -0300

    Practico N0 finalizado

--- practico0/promedio3.js
+++ practico0/promedio3.js
@@ -3,7 +3,7 @@ let num2 = -5.3;
 let num3 = -3.3;
 let promedio3= (num1+num2+num3)/3;
 console.log(promedio3);
-/**pregunta a 7
- * pregunta b 4.33
- * pregunta c 3.26
+/**pregunta a: 7
+ * pregunta b: 4.33
+ * pregunta c: 3.26
  */
