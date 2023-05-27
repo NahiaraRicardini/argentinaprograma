538de5c73c4df1489e3657d32ddeac8455d15728
Nahiara Ricardini <134017331+NahiaraRicardini@users.noreply.github.com>
2023-05-27 11:16:30 -0300

    Practico N0 finalizado

--- practico0/areaRectangulo.js
+++ practico0/areaRectangulo.js
@@ -2,7 +2,7 @@ let base = 12.5;
 let altura = 5.3;
 let areaRectangulo= base * altura;
 console.log(areaRectangulo);
-/** pregunta a 15
+/** pregunta a: 15
  * pregunta b: 66.25
  * 
 */
--- practico0/conversionGrados.js
+++ practico0/conversionGrados.js
@@ -3,5 +3,5 @@ let num2 = (num1 - 32)* 5/9;
 let conversionGrados= num2;
 console.log(conversionGrados);
 /**pregunta a:  10
- * pregunta b 23.88
+ * pregunta b: 23.88
 */
