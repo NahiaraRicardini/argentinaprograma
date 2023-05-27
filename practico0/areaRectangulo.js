83615f4654f2242f83bd9f97b6c46aad6d35647d
Nahiara Ricardini <134017331+NahiaraRicardini@users.noreply.github.com>
2023-05-27 11:17:51 -0300

    Practico N0 finalizado

--- practico0/areaRectangulo.js
+++ practico0/areaRectangulo.js
@@ -2,7 +2,7 @@ let base = 12.5;
 let altura = 5.3;
 let areaRectangulo= base * altura;
 console.log(areaRectangulo);
-/** pregunta a: 15
- * pregunta b: 66.25
+/** pregunta A: 15
+ * pregunta B: 66.25
  * 
 */
