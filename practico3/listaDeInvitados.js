13409bfaa93cd422192351422b5bd3bad4d3cbc4
Nahiara Ricardini <134017331+NahiaraRicardini@users.noreply.github.com>
2023-06-22 20:50:55 -0300

    Practico3

---
+++ practico3/listaDeInvitados.js
@@ -0,0 +1,26 @@
+let rechazados= []
+let admitidos = []
+let invitados = ["Maria","Juan", "Andres", "Lionel", "Leandro","Emilia","Jose" ,"Sofia" ]
+console.log ("La lista de invitados es: " + invitados)
+
+
+for (let i=0; i<invitados.length; i++){
+ let nombre = invitados [i]
+  if(nombre === "Jose" || nombre === "Sofia"){
+    rechazados.push (nombre);}
+    else {
+      admitidos.push(nombre);
+    }
+  }
+
+  for (let i=0; i<invitados.length; i++){
+    if(i = rechazados){
+      console.log ("La lista de invitados rechazados es: " + rechazados);
+    }
+  }
+    for (let i=0; i<invitados.length; i++){
+      if(i = admitidos){
+        console.log ("La lista de invitados admitidos es: " + admitidos);
+      }
+    }
+    
